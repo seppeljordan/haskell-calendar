6e709057f7dc2c389b364f14cb0168b553531592
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2015-04-21 17:24:30 +0200

    Add nix environment

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ stdenv, haskellngPackages }:
+
+let
+  env = haskellngPackages.ghcWithPackages ( p: with p; [
+    base
+    time
+    configurator
+  ]);
+in
+  stdenv.mkDerivation {
+    name = "hackandslay";
+    buildInputs = [env];
+    shellHook   = ''
+      export NIX_GHC="${env}/bin/ghc"
+      export NIX_GHCPKG="${env}/bin/ghc-pkg"
+      export NIX_GHC_DOCDIR="${env}/share/doc/ghc/html"
+      export NIX_GHC_LIBDIR=$( $NIX_GHC --print-libdir )
+   '';
+}
+					    
